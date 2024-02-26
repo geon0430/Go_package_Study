632d00b1de2ef3ec6393d679d5d291b7f462a3c8
Geon <114966864+geon0430@users.noreply.github.com>
2024-02-26 20:49:03 +0900

    Update logrus.go

--- logger/logrus.go
+++ logger/logrus.go
@@ -4,24 +4,22 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
+	"strconv"
 	"time"
+	"context"
 
 	"github.com/sirupsen/logrus"
 )
 
 type CustomFormatter struct{}
 
-var (
-	file      string
-	line      int
-	fileIndex int
-)
-
 func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	timestamp := entry.Time.Format("2006-01-02 15:04:05.000000000")
 	logLevel := strings.ToUpper(entry.Level.String())
+
+	var file string
+	var line int
 
 	if entry.HasCaller() {
 		file = filepath.Base(entry.Caller.File)
@@ -32,8 +30,12 @@ func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	return []byte(msg), nil
 }
 
-func SetupLogging(logPath, logLevel, name string) *logrus.Logger {
+func SetupLogging(ctx context.Context, logPath, model, name, logLevel, pipelineID string) *logrus.Logger {
 	logLevel = strings.ToUpper(logLevel)
+	maxSizeMB := int64(100)
+	maxFiles := 10
+
+
 	if logPath == "" {
 		logPath = "/tmp/log"
 	}
@@ -43,7 +45,7 @@ func SetupLogging(logPath, logLevel, name string) *logrus.Logger {
 		logrus.Fatalf("Failed to create log directory: %v", err)
 	}
 
-	logFileName := fmt.Sprintf("%s_%s_go.log", name, logLevel)
+	logFileName := fmt.Sprintf("%s_%s_%s_go.log", name, pipelineID, logLevel)
 	logFilePath := filepath.Join(logDir, logFileName)
 
 	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -56,7 +58,6 @@ func SetupLogging(logPath, logLevel, name string) *logrus.Logger {
 	logger.SetFormatter(new(CustomFormatter))
 	logger.SetReportCaller(true)
 
-	// Setup log level
 	switch logLevel {
 	case "DEBUG":
 		logger.SetLevel(logrus.DebugLevel)
@@ -72,8 +73,8 @@ func SetupLogging(logPath, logLevel, name string) *logrus.Logger {
 		logger.SetLevel(logrus.InfoLevel)
 	}
 
-	// Log file rotation
-	go rotateLogFile(logger, file, logFilePath, 10)
+	go rotateLogFile(ctx, logger, logFilePath,maxSizeMB, maxFiles)
+
 
 	return logger
 }
@@ -108,52 +109,60 @@ func parseLogIndex(fileName string) (int, error) {
 	ext := filepath.Ext(fileName)
 	base = strings.TrimSuffix(base, ext)
 	parts := strings.Split(base, "_")
+	
 	if len(parts) > 1 {
 		return strconv.Atoi(parts[len(parts)-1])
 	}
 	return 0, fmt.Errorf("log file name does not contain index")
 }
-func rotateLogFile(logger *logrus.Logger, file *os.File, basePath string, maxSizeMB int64) {
-	maxIndex := 10
 
-	for {
-		time.Sleep(1 * time.Minute)
+func rotateLogFile(ctx context.Context, logger *logrus.Logger, logFilePath string, maxSizeMB int64, maxFiles int) {
+    checkLogDuration := 1 * time.Second
+    checkSizeTicker := time.NewTicker(checkLogDuration)
+    defer checkSizeTicker.Stop()
 
-		fileInfo, err := os.Stat(basePath)
-		if err != nil {
-			if os.IsNotExist(err) {
-				continue
-			}
-			logger.Fatalf("Failed to stat log file: %v", err)
-		}
+    for {
+        select {
+        case <-ctx.Done(): // 종료 신호를 받음
+            logger.Info("Log rotation stopped due to context cancellation")
+            return
+        case <-checkSizeTicker.C:
+            fileInfo, err := os.Stat(logFilePath)
+            if err != nil {
+                logger.Errorf("Failed to get log file info: %v", err)
+                continue
+            }
 
-		if fileInfo.Size() > maxSizeMB*1024*1024 {
-			file.Close()
+            if fileInfo.Size() < maxSizeMB*1024*1024 {
+                continue
+            }
 
-			for i := maxIndex - 1; i >= 0; i-- {
-				oldPath := incrementLogFileName(basePath, i)
-				newPath := incrementLogFileName(basePath, i+1)
-				if _, err := os.Stat(oldPath); err == nil {
-					if i == (maxIndex - 1) {
-						os.Remove(oldPath)
-					} else {
-						os.Rename(oldPath, newPath)
-					}
-				}
-			}
+            for i := maxFiles - 1; i > 0; i-- {
+                oldPath := fmt.Sprintf("%s.%d", logFilePath, i-1)
+                if i == 1 {
+                    oldPath = logFilePath
+                }
+                newPath := fmt.Sprintf("%s.%d", logFilePath, i)
 
-			os.Rename(basePath, incrementLogFileName(basePath, 1))
+                if _, err := os.Stat(oldPath); os.IsNotExist(err) {
+                    continue
+                }
+                os.Rename(oldPath, newPath)
+            }
 
-			newFile, err := os.OpenFile(basePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-			if err != nil {
-				logger.Fatalf("Failed to open new log file: %v", err)
-			}
-
-			logger.SetOutput(newFile)
-			file = newFile
-		}
-	}
+            os.Truncate(logFilePath, 0)
+            file, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+            if err != nil {
+                logger.Errorf("Failed to open new log file: %v", err)
+                return
+            }
+            logger.SetOutput(file)
+        }
+    }
 }
+
+
+
 
 func incrementLogFileName(basePath string, index int) string {
 	dir, file := filepath.Split(basePath)
@@ -170,4 +179,5 @@ func resetFileIndexIfNecessary(currentIndex, maxIndex int) int {
 	}
 	return currentIndex + 1
 }
+
 
